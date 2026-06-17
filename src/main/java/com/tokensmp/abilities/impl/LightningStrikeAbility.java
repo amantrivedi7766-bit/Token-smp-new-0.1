7d18768ebc538c1186dcf0552a75c4ea3392de35
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-17 08:09:30 +0530

    Update LightningStrikeAbility.java

--- src/main/java/com/tokensmp/abilities/impl/LightningStrikeAbility.java
+++ src/main/java/com/tokensmp/abilities/impl/LightningStrikeAbility.java
@@ -19,10 +19,8 @@ public class LightningStrikeAbility extends AbstractAbility {
         double damage = scaleDamage(8, level);
         Location target = player.getTargetBlock(null, 50).getLocation().add(0.5, 0, 0.5);
         player.getWorld().strikeLightningEffect(target);
-        for (LivingEntity e : player.getWorld().getNearbyEntities(target, 3, 3, 3, e -> e instanceof LivingEntity && e != player)) {
-            ((LivingEntity) e).damage(damage, player);
-        }
-        // More particles at higher levels
+        player.getWorld().getNearbyEntities(target, 3, 3, 3, e -> e instanceof LivingEntity && e != player)
+                .forEach(e -> ((LivingEntity) e).damage(damage, player));
         int count = 10 + level * 5;
         player.getWorld().spawnParticle(Particle.DUST, target, count, 0.5, 0.5, 0.5, new org.bukkit.Particle.DustOptions(org.bukkit.Color.ORANGE, 1));
         player.getWorld().playSound(target, Sound.ENTITY_LIGHTNING_BOLT_THUNDER, 1.0f, 0.8f - (level-1)*0.05f);
