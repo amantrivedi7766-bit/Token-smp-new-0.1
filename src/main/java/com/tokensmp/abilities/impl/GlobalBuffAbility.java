32383a097de72c9a479315d02b5bc8b9299b14de
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-17 09:45:12 +0530

    Implement GlobalBuffAbility to enhance player effects

---
+++ src/main/java/com/tokensmp/abilities/impl/GlobalBuffAbility.java
@@ -0,0 +1,24 @@
+package com.tokensmp.abilities.impl;
+
+import com.tokensmp.abilities.AbstractAbility;
+import com.tokensmp.tokens.Token;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class GlobalBuffAbility extends AbstractAbility {
+    public GlobalBuffAbility() {
+        super("Global Buff", "Gives all players speed and strength.", 60);
+    }
+
+    @Override
+    public void execute(Player player, Token token) {
+        int level = getLevel(player, token);
+        int duration = scaleDuration(160, level);
+        int amp = level - 1;
+        player.getWorld().getPlayers().forEach(p -> {
+            p.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, duration, 1 + amp));
+            p.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, duration, 1 + amp));
+        });
+    }
+}
