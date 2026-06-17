cfbbef8f1f19fd019bbb58d3a957dd75c25ac68b
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-17 09:42:07 +0530

    Create MeteorStrikeAbility.java

---
+++ src/main/java/com/tokensmp/abilities/impl/MeteorStrikeAbility.java
@@ -0,0 +1,30 @@
+package com.tokensmp.abilities.impl;
+
+import com.tokensmp.abilities.AbstractAbility;
+import com.tokensmp.tokens.Token;
+import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+
+public class MeteorStrikeAbility extends AbstractAbility {
+    public MeteorStrikeAbility() {
+        super("Meteor Strike", "Calls a meteor dealing massive damage.", 70);
+    }
+
+    @Override
+    public void execute(Player player, Token token) {
+        int level = getLevel(player, token);
+        float explosionPower = 3.0f + (level - 1) * 0.5f;
+        double damage = scaleDamage(10, level);
+        double radius = scaleRadius(4, level);
+        Location target = player.getTargetBlock(null, 50).getLocation().add(0.5, 0, 0.5);
+        target.getWorld().createExplosion(target, explosionPower, true, true);
+        target.getWorld().spawnParticle(Particle.EXPLOSION_LARGE, target, 5 + level*3);
+        target.getWorld().playSound(target, Sound.ENTITY_GENERIC_EXPLODE, 1.0f, 0.7f - (level-1)*0.05f);
+        target.getWorld().getNearbyEntities(target, radius, radius, radius,
+                e -> e instanceof LivingEntity && e != player)
+                .forEach(e -> ((LivingEntity) e).damage(damage, player));
+    }
+}
