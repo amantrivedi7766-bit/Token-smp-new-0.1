74237eb40e013d9ce33a8e808fda7543b9c79ca3
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-17 07:27:03 +0530

    Enhance StormSummonAbility with level-based effects

--- src/main/java/com/tokensmp/abilities/impl/StormSummonAbility.java
+++ src/main/java/com/tokensmp/abilities/impl/StormSummonAbility.java
@@ -1,6 +1,7 @@
 package com.tokensmp.abilities.impl;
 
 import com.tokensmp.abilities.AbstractAbility;
+import com.tokensmp.tokens.Token;
 import org.bukkit.Location;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
@@ -10,15 +11,19 @@ public class StormSummonAbility extends AbstractAbility {
     public StormSummonAbility() {
         super("Storm Summon", "Summons a thunderstorm around you.", 60);
     }
+
     @Override
-    public void execute(Player player, com.tokensmp.tokens.Token token) {
+    public void execute(Player player, Token token) {
+        int level = getLevel(player, token);
         Location center = player.getLocation();
         player.getWorld().setStorm(true);
         player.getWorld().setThundering(true);
-        for (int i = 0; i < 20; i++) {
-            Location loc = center.clone().add(Math.random()*10-5, 10, Math.random()*10-5);
+        int strikes = 10 + level * 5;
+        for (int i = 0; i < strikes; i++) {
+            Location loc = center.clone().add(Math.random()*12-6, 10, Math.random()*12-6);
             player.getWorld().strikeLightningEffect(loc);
         }
-        player.getWorld().playSound(center, Sound.AMBIENT_WEATHER_THUNDER, 1.0f, 0.5f);
+        player.getWorld().spawnParticle(Particle.CLOUD, center, 30 + level*10, 5, 2, 5);
+        player.getWorld().playSound(center, Sound.AMBIENT_WEATHER_THUNDER, 1.0f, 0.5f - (level-1)*0.05f);
     }
 }
