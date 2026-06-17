688129efce554599662ef3546395b62d16c49941
amantrivedi7766-bit <amantrivedi7766@gmail.com>
2026-06-17 07:42:49 +0530

    Update MessageUtil.java

---
+++ src/main/java/com/tokensmp/utils/MessageUtil.java
@@ -0,0 +1,9 @@
+package com.tokensmp.utils;
+
+import org.bukkit.ChatColor;
+
+public class MessageUtil {
+    public static String color(String msg) {
+        return ChatColor.translateAlternateColorCodes('&', msg);
+    }
+}
